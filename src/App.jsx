832aa88dfecf600848aa62fe15846898567673e6
Samuel <ajiboyeadebayo901@gmail.com>
2025-06-09 13:49:01 +0100

    Basic UI components

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,90 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+// import { nav } from './constants'
+import { MdDashboard } from "react-icons/md";
+import { RiDiscussFill } from "react-icons/ri";
+import { MdQuiz } from "react-icons/md";
+import { RiAccountCircle2Fill } from "react-icons/ri";
+import { IoMdSettings } from "react-icons/io";
+import { completed, recommended, upcoming } from './constants';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    <div className='grid-container'>
+
+      <nav className='sidebar'>
+        <div className='nav-cont'>
+          <div class="nav-cont"><h2>GemAcademy.</h2>
+          <a href="#" className="link"><MdDashboard />
+          <div>Dashboard</div></a>
+          <a href="#" className="link"><RiDiscussFill /><div>Discussion</div></a>
+          <a href="#" className="link"><MdQuiz /><div>Quiz</div></a>
+          <a href="#" className="link"><RiAccountCircle2Fill /><div>My Account</div></a>
+          <a href="#" className="link"><IoMdSettings /><div>Setting</div></a>
+          </div>
+        </div>
+      </nav>
+
+      <main>
+
+        <div className='upcoming'>
+          <h2>Upcoming Task</h2>
+          <div className='layout'>
+            {upcoming.map((task)=>{
+              return(
+                <div className='upcoming-task-card' key={task.title}>
+                  <div>
+                    <h3>{task.title}</h3>
+                    <span><p>Timeframe - {task.timeframe}</p></span>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+
+
+        <div className='recommendation'>
+           <h2>Recommmended Courses</h2>
+          {recommended.map((course)=>{
+            return(
+              <div className='recommended-course-card' key={course.title}>
+                <div className='img-cont'>
+                  <img src={course.imgUrl} alt="course thumbnail" />
+                </div>
+                <div>
+                  <h3>{course.title}</h3>
+                  <span><p>{course.tutor}</p></span>•
+                  <span><p> {course.totalHr}hr</p></span>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+
+        <div className='completed'>
+          <h2>Completed Courses</h2>
+          {completed.map((course)=>{
+            return(
+              <div className='completed-course-card' key={course.title}>
+                <div className='img-cont'>
+                  <img src={course.imgUrl} alt="course thumbnail" />
+                </div>
+                <div>
+                  <h3>{course.title}</h3>
+                  <span><p>{course.tutor}</p></span>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </main>
+    
+    </div>
   )
 }
 
---
+++ src/constants/index.js
@@ -0,0 +1,72 @@
+// export const nav = [
+//     {
+//         icon: "",
+//         name: "Dashboard",
+//         href: "#",
+//     },
+//     {
+//         icon: "",
+//         name: "Discussion",
+//         href: "#",
+//     },
+//     {
+//         icon: "",
+//         name: "Qiuz",
+//         href: "#",
+//     },
+//     {
+//         icon: "",
+//         name: "My Account",
+//         href: "#",
+//     },
+//     {
+//         icon: "",
+//         name: "Setting",
+//         href: "#",
+//     },
+// ];
+
+export const completed = [
+    {
+        imgUrl: "https://velog.velcdn.com/images/dhlee91/post/f2418376-eacc-40cb-b7c8-73473580da3e/html_.png",
+        title: "HTML",
+        tutor: "Samuel Ajiboye",
+        totalHr: "2",
+    },
+    {
+        imgUrl: "https://www.excellencetechnology.in/wp-content/uploads/2024/01/CSS-Tutorial.webp",
+        title: "CSS",
+        tutor: "Ruth Godwin",
+        totalHr: "4",
+    },
+];
+export const recommended = [
+    {
+        imgUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Javascript.jpg/960px-Javascript.jpg?20210623121127",
+        title: "JavaScript",
+        tutor: "Samuel Ajiboye",
+        totalHr: "2",
+    },
+    {
+        imgUrl: "https://media.licdn.com/dms/image/v2/D4D12AQF5LKYv_4CoYw/article-cover_image-shrink_600_2000/article-cover_image-shrink_600_2000/0/1695497781676?e=2147483647&v=beta&t=aSGqvPXSceClfW8UfA3jzQVjHg4_JRxb5UW9Fbc0cOQ",
+        title: "React.js",
+        tutor: "Ruth Godwin",
+        totalHr: "4",
+    },
+    {
+        imgUrl: "https://cdn.slidesharecdn.com/ss_thumbnails/tailwindcss-220708140925-2a85561f-thumbnail.jpg?width=640&height=640&fit=bounds",
+        title: "TailwindCSS",
+        tutor: "Ruth Godwin",
+        totalHr: "4",
+    },
+];
+export const upcoming = [
+    {
+        title: "Build a portfolio website",
+        timeframe: "2 days",
+    },
+    {
+        title: "Build a travel blog website",
+        timeframe: "2 days",
+    },
+];
